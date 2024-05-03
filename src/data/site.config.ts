3e22f01fbbdca41a0307c321c229690bee9948df
Dilan <dilan.p.2020.ad@ritchennai.edu.in>
2024-05-03 17:10:09 +0530

    initial commit

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,9 +9,9 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
+	author: 'Dilan Poulous Euke', // Site author
+	title: 'Hunter Blog', // Site title.
+	description: 'Average guy with knack for all things', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
