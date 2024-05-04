fcf7e982d55392a5cf75dc7aef5d861497709c69
Dilan <dilan.p.2020.ad@ritchennai.edu.in>
2024-05-04 20:06:26 +0530

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,32 +2,33 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import { remarkReadingTime } from './src/utils/readTime.ts'
+import remarkReadingTime from 'remark-reading-time'
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://hunterblog.netlify.app/', // Write here your website url
-	markdown: {
-		remarkPlugins: [remarkReadingTime],
-		drafts: true,
-		shikiConfig: {
-			theme: 'material-theme-palenight',
-			wrap: true
-		}
-	},
-	integrations: [
-		mdx({
-			syntaxHighlight: 'shiki',
-			shikiConfig: {
-				experimentalThemes: {
-					light: 'vitesse-light',
-					dark: 'material-theme-palenight',
-				  },
-				wrap: true
-			},
-			drafts: true
-		}),
-		sitemap(),
-		tailwind()
-	]
+  site: 'https://hunterblog.netlify.app/', // Write here your website url
+  integrations: [
+    mdx({
+      syntaxHighlight: 'shiki',
+      shikiConfig: {
+        experimentalThemes: {
+          light: 'vitesse-light',
+          dark: 'material-theme-palenight',
+        },
+        wrap: true
+      },
+      drafts: true
+    }),
+    sitemap(),
+    tailwind()
+  ],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+    markdownItOptions: {
+      html: true, // Enable HTML tags in Markdown
+    },
+    shiki: {
+      theme: 'material-theme-palenight',
+      wrap: true,
+    },
+  },
 })
