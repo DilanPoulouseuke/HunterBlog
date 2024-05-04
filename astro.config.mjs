9ff79674e7102a483e3d5ec4a8cba9cd51a70d88
Dilan <dilan.p.2020.ad@ritchennai.edu.in>
2024-05-04 21:03:27 +0530

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,29 +4,30 @@ import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import { remarkReadingTime } from './src/utils/readTime.ts'
 
+// https://astro.build/config
 export default defineConfig({
-    site: 'https://hunterblog.netlify.app/',
-    markdown: {
-        remarkPlugins: [[remarkReadingTime, {}]], // Correct format for remarkPlugins
-        drafts: true,
-        shikiConfig: {
-            theme: 'material-theme-palenight',
-            wrap: true
-        }
-    },
-    integrations: [
-        mdx({
-            syntaxHighlight: 'shiki',
-            shikiConfig: {
-                experimentalThemes: {
-                    light: 'vitesse-light',
-                    dark: 'material-theme-palenight',
-                },
-                wrap: true
-            },
-            drafts: true
-        }),
-        sitemap(),
-        tailwind()
-    ]
+	site: 'https://hunterblog.netlify.app/', // Write here your website url
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+		drafts: true,
+		shikiConfig: {
+			theme: 'material-theme-palenight',
+			wrap: true
+		}
+	},
+	integrations: [
+		mdx({
+			syntaxHighlight: 'shiki',
+			shikiConfig: {
+				experimentalThemes: {
+					light: 'vitesse-light',
+					dark: 'material-theme-palenight',
+				  },
+				wrap: true
+			},
+			drafts: true
+		}),
+		sitemap(),
+		tailwind()
+	]
 })
